43578688cae47b517db09a368746834b49f91065
Michel Käser <mail@michelkaeser.ch>
2015-06-19 23:02:50 +0200

    refactored interface...was stupid

--- src/Gatherers/BinaryVersionsGatherer.php
+++ src/Gatherers/BinaryVersionsGatherer.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace BinaryMngr\Contract;
+namespace BinaryMngr\Contract\Gatherers;
 
 /**
- * The BinaryVersionGatherer interface is used to check if new versions of
+ * The BinaryVersionsGatherer interface is used to check if new versions of
  * binaries exist. A typical example would be some kind of 'GithubTagsGatherer'
  * that looks-up versions based on the tags page on Github.
  *
@@ -13,9 +13,9 @@ namespace BinaryMngr\Contract;
  * will create user messages for outdated binary versions etc.
  *
  * Note: All exceptions thrown in here need to be of kind
- * \BinaryMngr\Contract\BinaryVersionGathererException;
+ * \BinaryMngr\Contract\BinaryVersionsGathererException;
  */
-interface BinaryVersionGatherer
+interface BinaryVersionsGatherer
 {
     /**
      * Constants for the keys the returned associative arrays should include.
@@ -30,21 +30,24 @@ interface BinaryVersionGatherer
     /**
      * Performs the actual gathering by fetching the binary versions from
      * the external source.
-     *
-     * If $identifier isn't null, it represents the identifier of the last known
-     * binary version that is already present in the application. If possible, the
-     * gatherer should not gather versions 'older' than this one.
-     *
-     * @param string $identifier the last known version identifier
      */
-    public function gather($identifier = null);
+    public function gather();
 
     /**
      * Returns the latest version of all gathered binary versions.
      *
+     * If no such version exists, null should be returned.
+     *
      * @return array an associative array containing the const fields defined in this interface
      */
     public function getLatestVersion();
+
+    /**
+     * Returns the gatherer's name (e.g. GitHubsTagsGatherer)
+     *
+     * @return string
+     */
+    public function getName();
 
     /**
      * Returns all binary versions the last gather() call recognized.
@@ -52,4 +55,13 @@ interface BinaryVersionGatherer
      * @return array all gathered binary versions
      */
     public function getVersions();
+
+    /**
+     * Sets the binary for which versions should be gathered.
+     *
+     * @param array $binary an array describing the binary ($binary->toArray())
+     *
+     * @return \BinaryMngr\Contract\Gatherers\BinaryVersionsGatherer $this
+     */
+    public function setBinary(array $binary);
 }
---
+++ src/Gatherers/BinaryVersionsGathererException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BinaryMngr\Contract\Gatherers;
+
+use Exception;
+
+/**
+ * Base class for all exceptions thrown in BinaryVersionsGatherer implementations.
+ */
+class BinaryVersionsGathererException extends Exception {}
